c6216ab63509c79d2eb20c4dd4cdab7a5de003a9
OpenShift guest <jackliang1987@hotmail.com>
2013-04-13 20:48:28 -0400

    another one

--- myproject/myproject/static/calendar.js
+++ myproject/myproject/static/calendar.js
@@ -1,17 +1,32 @@
 $(document).ready(function() {
  // This is more like it!
      alert("sad");
-     $("td").hover( 
+     
+     $(".day").hover( 
         function() {
         var id = $(this).data('id');
+        $(this).css( "background-color", "rgba(245,200,200,0.3)" );
         $('.edit-icon-'+id).stop(true, true).hide().css({
           visibility: "visible"
         }).fadeIn("slow");
         return this;
     }, function() {
-         var id = $(this).data('id');
-        return $('.edit-icon-'+id).fadeOut("fast", function() {
-        return $('.edit-icon-'+id).show().css({ visibility: "hidden" });
+        var id = $(this).data('id');
+        $(this).css( "background-color", "rgba(200,255,200,0.3)" );
+        return $('.edit-icon-'+id).css({ visibility: "hidden" });
       });
-    });
+      
+    $(".current").hover( 
+        function() {
+        var id = $(this).data('id');
+        $(this).css( "background-color", "rgba(245,200,200,0.3)" );
+        $('.edit-icon-'+id).stop(true, true).hide().css({
+          visibility: "visible"
+        }).fadeIn("slow");
+        return this;
+    }, function() {
+        var id = $(this).data('id');
+        $(this).css( "background-color", "rgba(200,200,255,0.5)" );
+        return $('.edit-icon-'+id).css({ visibility: "hidden" });
+      });
 });
