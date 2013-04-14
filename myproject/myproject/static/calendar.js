b86ee0a6620ba8b343f6ffe99949210189660561
OpenShift guest <jackliang1987@hotmail.com>
2013-04-14 14:46:59 -0400

    pencil

--- myproject/myproject/static/calendar.js
+++ myproject/myproject/static/calendar.js
@@ -1,32 +1,50 @@
+$.fn.showEdit = function(id) {
+  $(document).on('click', '#pencil-'+id, (function(event) { $(this).popover('show'); event.preventDefault(); }));
+};
+
+$.fn.hideEdit = function(id) {
+  $('#pencil-'+id).popover('hide');
+};
+
+
 $(document).ready(function() {
- // This is more like it!
-     alert("sad");
-     
      $(".day").hover( 
         function() {
         var id = $(this).data('id');
-        $(this).css( "background-color", "rgba(245,200,200,0.3)" );
+        $(this).css( "background-color", "rgba(200,255,200,0.25)" );
+        $(this).showEdit(id);
         $('.edit-icon-'+id).stop(true, true).hide().css({
           visibility: "visible"
-        }).fadeIn("slow");
+        }).fadeIn('slow');
+        $('.pencil-icon-'+id).stop(true, true).hide().css({
+          visibility: "visible"
+        }).fadeIn('slow');
         return this;
     }, function() {
         var id = $(this).data('id');
+        $(this).hideEdit(id);
         $(this).css( "background-color", "rgba(200,255,200,0.3)" );
-        return $('.edit-icon-'+id).css({ visibility: "hidden" });
+        $('.edit-icon-'+id).css({ visibility: "hidden" });
+        $('.pencil-icon-'+id).css({ visibility: "hidden" });
+        return this;
       });
       
     $(".current").hover( 
         function() {
         var id = $(this).data('id');
-        $(this).css( "background-color", "rgba(245,200,200,0.3)" );
+        $(this).css( "background-color", "rgba(200,200,255,0.45)" );
         $('.edit-icon-'+id).stop(true, true).hide().css({
           visibility: "visible"
-        }).fadeIn("slow");
+        }).fadeIn('slow');
+         $('.pencil-icon-'+id).stop(true, true).hide().css({
+          visibility: "visible"
+        }).fadeIn('slow');
         return this;
     }, function() {
         var id = $(this).data('id');
         $(this).css( "background-color", "rgba(200,200,255,0.5)" );
-        return $('.edit-icon-'+id).css({ visibility: "hidden" });
+        $('.edit-icon-'+id).css({ visibility: "hidden" });
+        $('.pencil-icon-'+id).css({ visibility: "hidden" });
+        return this;
       });
 });
