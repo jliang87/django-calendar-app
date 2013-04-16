cb080a1f6a9a64d9a20ab1a72c0da27ac1187926
OpenShift guest <jackliang1987@hotmail.com>
2013-04-16 01:55:37 -0400

    css

--- myproject/myproject/static/calendar.js
+++ myproject/myproject/static/calendar.js
@@ -1,50 +1,56 @@
 $.fn.showEdit = function(id) {
-  $(document).on('click', '#pencil-'+id, (function(event) { $('#td-'+id).css("overflow", "visible"); $(this).popover('show', {html:'true'}); event.preventDefault(); }));
+  $(document).on('click', '#pencil-'+id, (function(event) { event.preventDefault(); }));
 };
 
 $.fn.hideEdit = function(id) {
-  $('#td-'+id).css("overflow-y", "scroll");
-  $('#pencil-'+id).popover('destroy', {html:'false'});
 };
 
 $(document).ready(function() {
      $(".day").hover( 
         function() {
         var id = $(this).data('id');
-        $(this).css( "background-color", "rgba(200,255,200,0.25)" );
+        $(this).css( "background-color", "rgba(255,255,255,0.35)" );
         $(this).showEdit(id);
-        $('.edit-icon-'+id).stop(true, true).hide().css({
+        $('#edit-icon-'+id).stop(true, true).hide().css({
           visibility: "visible"
         }).fadeIn('slow');
-        $('.pencil-icon-'+id).stop(true, true).hide().css({
+        $('#pencil-icon-'+id).stop(true, true).hide().css({
           visibility: "visible"
         }).fadeIn('slow');
+        $('#star-icon-'+id).css({ visibility: "hidden" });
         return this;
     }, function() {
         var id = $(this).data('id');
         $(this).hideEdit(id);
-        $(this).css( "background-color", "rgba(200,255,200,0.3)" );
-        $('.edit-icon-'+id).css({ visibility: "hidden" });
-        $('.pencil-icon-'+id).css({ visibility: "hidden" });
+        $(this).css( "background-color", "rgba(255,255,255,0.4)" );
+        $('#edit-icon-'+id).css({ visibility: "hidden" });
+        $('#pencil-icon-'+id).css({ visibility: "hidden" });
+        $('#star-icon-'+id).stop(true, true).hide().css({
+          visibility: "visible"
+        }).fadeIn('fast');
         return this;
       });
       
     $(".current").hover( 
         function() {
         var id = $(this).data('id');
-        $(this).css( "background-color", "rgba(200,200,255,0.45)" );
-        $('.edit-icon-'+id).stop(true, true).hide().css({
+        $(this).css( "background-color", "rgba(255,255,255,0.35)" );
+        $('#edit-icon-'+id).stop(true, true).hide().css({
           visibility: "visible"
         }).fadeIn('slow');
-         $('.pencil-icon-'+id).stop(true, true).hide().css({
+        $('#pencil-icon-'+id).stop(true, true).hide().css({
           visibility: "visible"
         }).fadeIn('slow');
+        $('#star-icon-'+id).css({ visibility: "hidden" });
         return this;
     }, function() {
         var id = $(this).data('id');
-        $(this).css( "background-color", "rgba(200,200,255,0.5)" );
-        $('.edit-icon-'+id).css({ visibility: "hidden" });
-        $('.pencil-icon-'+id).css({ visibility: "hidden" });
+        $(this).css( "background-color", "rgba(255,255,255,0.4)" );
+        $('#edit-icon-'+id).css({ visibility: "hidden" });
+        $('#pencil-icon-'+id).css({ visibility: "hidden" });
+        $('#star-icon-'+id).stop(true, true).hide().css({
+          visibility: "visible"
+        }).fadeIn('fast');
         return this;
       });
 });
